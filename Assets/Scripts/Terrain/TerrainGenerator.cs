ebc5dbb1cbec73a71692834ae0c80506a3cc185e
Nico Smith <web@niiccoo2.xyz>
2025-12-27 09:43:38 -0500

    Add color

--- Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -48,6 +48,8 @@ using System.Diagnostics.Contracts;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColorMap, Mesh};
+    public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -60,12 +62,43 @@ public class TerrainGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateTerrain()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {   
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
         TerrainDisplay display = FindAnyObjectByType<TerrainDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenarator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColorMap)
+        {
+            display.DrawTexture(TextureGenarator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+        // } else if (drawMode == DrawMode.Mesh)
+        // {
+        //     display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), );
+        // }
+        
     }
 
     void OnValidate()
@@ -78,4 +111,12 @@ public class TerrainGenerator : MonoBehaviour
 
         if (octaves < 0) octaves = 0;
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }
